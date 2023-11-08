f9dff0679a7596637b46e835c7748e532dbc713f
Henry Zaks <100029318@mvla.net>
2023-11-07 22:14:37 -0800

    Passes JUnit

--- src/ExpressionEvaluator.java
+++ src/ExpressionEvaluator.java
@@ -8,8 +8,34 @@ public class ExpressionEvaluator {
 	private static final String DIV0_ERROR = "Div0 Error: ";
 
 	// The placeholder for the two stacks
+
 	private GenericStack<Double> dataStack;
 	private GenericStack<String>  operStack;
+	
+	/**
+	 * Returns true if the first string has higher precedence
+	 *
+	 * @param str1 and str2
+	 * @return whether or not str1 has precedence over str2
+	 */
+	private boolean isHigherPrecedence(String str1, String str2) {
+		if (str1.equals("*") || str1.equals("/")) {
+			if (str2.equals("(")) {
+				return false;
+			}
+			return true;
+		}
+		if (str1.equals("+") || str1.equals("-")) {
+			if (str2.equals("(") || str2.equals("*") || str2.equals("/")) {
+				return false;
+			}
+			return true;
+		}
+		if (str2.equals(")")) {
+			return true;
+		}
+		return false;
+	}
 	
 	/**
 	 * Convert to tokens. Takes a string and splits it into tokens that
@@ -22,7 +48,156 @@ public class ExpressionEvaluator {
 	 * @return the string[] of tokens
 	 */
 	private String[] convertToTokens(String str) {
-		return null;
+		str = str.replaceAll("^(\\s*)\\-([\\d\\.])", "$1NEG$2");
+		str = str.replaceAll("([\\+\\-\\*\\/\\(]\\s*)\\-([\\d\\.])", "$1 NEG$2");
+		str = str.replaceAll("([\\+\\-\\*\\/\\(\\)])", " $1 ");
+		str = str.trim();
+		str = str.replaceAll("\\)\\s+([\\d\\(\\.])", ") * $1");
+		str = str.replaceAll("([\\d\\.])\\s+\\(", "$1 * (");
+		str = str.replaceAll("(^\\s*|[\\(\\+\\-\\/\\*])\\s*\\-\\s+(\\()", "$1 = $2");
+		str = sub(str);
+		str = str.trim();
+		String[] tokens = str.split("\\s+");
+		tokens = removeNEG(tokens);
+		return tokens;
+	}
+	
+	/**
+	 * Removes all = signs with implicit multiplication
+	 *
+	 * @param String str
+	 * @return the updated String str
+	 */
+	private String sub(String str) {
+		int parenCount = 0;
+		boolean parenFound = false;
+		while (str.contains("=")) {
+			int startIndex = str.indexOf("=");
+			int endIndex = 14;
+			System.out.println(str);
+			parenCount = 0;
+			parenFound = false;
+			for (int i = startIndex; i < str.length(); i++) {
+				if (str.charAt(i) == '(') {
+					parenCount++;
+					parenFound = true;
+				}
+				if (str.charAt(i) == ')') {
+					parenCount--;
+				}
+				if (parenFound && parenCount == 0) {
+					endIndex = i;
+					break;
+				}
+			}
+			str = str.substring(0, startIndex) + "( NEG1 * " + str.substring(startIndex + 1, endIndex) + " ) " + str.substring(endIndex, str.length());
+		}
+		return str;
+	}
+	
+	/**
+	 * Removes NEG and replaces it with a negative sign
+	 *
+	 * @param String[] of tokens
+	 * @return new String[] of tokens
+	 */
+	private String[] removeNEG(String[] tokens) {
+		String[] removed = new String[tokens.length];
+		for (int i = 0; i < tokens.length; i++) {
+			String token = tokens[i];
+			if (token.length() >= 3 && token.substring(0, 3).equals("NEG")) {
+				token = token.replaceAll("NEG", "-");		
+			}
+			removed[i] = token;
+		}
+		return removed;
+	}
+	
+	
+	/**
+	 * Does error checking for DATA_ERROR, OP_ERROR, and PAREN_ERROR before attempting to evaluate the expression
+	 *
+	 * @param String[] of tokens
+	 * @return the error, or an empty string
+	 */
+	private String containsError(String[] tokens) {
+		int parenCount = 0;
+		if (tokens[0].matches("[\\+\\/\\*]") || tokens[tokens.length - 1].matches("[\\+\\/\\*\\-]"))
+			return OP_ERROR;
+		for (int i = 0; i < tokens.length; i++) {
+			String err = dataOppErrors(tokens, i);
+			if (err != "")
+				return err;
+			if (tokens[i].equals("(")) {
+				if (i == tokens.length - 1 || tokens[i + 1].matches("[\\+\\-\\*\\/\\)]"))
+					return PAREN_ERROR;
+				parenCount++;
+			}
+			if (tokens[i].equals(")"))
+				parenCount--;
+			if (parenCount < 0)
+				return PAREN_ERROR;
+		}
+		if (parenCount != 0)
+			return PAREN_ERROR;
+		return "";
+	}
+	
+	
+	/**
+	 * Does error checking for DATA_ERROR and OP_ERROR as a helper method for containsError
+	 *
+	 * @param String[] of tokens, index i
+	 * @return the error, or an empty string
+	 */
+	private String dataOppErrors(String[] tokens, int i) {
+		String token = tokens[i];
+		String next = "DNE";
+		String next2 = "DNE";
+		if (i < tokens.length - 1) {
+			if (i < tokens.length - 2)
+				next2 = tokens[i + 2];
+			next = tokens[i + 1];
+		}
+		if (!token.matches("[\\+\\-\\*\\/\\)\\(]")) {
+			try {
+				Double.parseDouble(token);
+			} catch (NumberFormatException e) {
+				return DATA_ERROR;
+			}
+			if (!token.matches("[\\d.-]+"))
+				return DATA_ERROR;
+			if (!next.equals("DNE") && !next.matches("[\\+\\-\\*\\/\\)\\(]"))
+				return DATA_ERROR;
+		}
+		if (!next.equals("DNE")) {
+			if (!nextExists(token, next, next2).equals("")) {
+				return nextExists(token, next, next2);
+			}
+		}
+		return "";
+	}
+	
+	/**
+	 * Does error checking for consecutive operations
+	 *
+	 * @param Strings: token, next token, token after next token
+	 * @return the error, or an empty string
+	 */
+	private String nextExists(String token, String next, String next2) {
+		if (token.matches("[\\+\\*\\/\\-]")) {
+			if (next.matches("[\\+\\*\\/\\)]"))
+				return OP_ERROR;
+		}
+		if (next.equals("-")) {
+			if (token.matches("[\\/\\*]"))
+				return OP_ERROR;
+		}
+		if (token.equals("(") && next.matches("[\\+\\*\\/\\-]"))
+			return OP_ERROR;
+		if (token.matches("[\\+\\-\\*\\/]") && next.matches("[\\+\\-\\*\\/]") && next2.matches("[\\+\\-\\*\\/]")) 
+			return OP_ERROR;
+		return "";
 	}
 	
 	/**
@@ -49,8 +224,82 @@ public class ExpressionEvaluator {
 	protected String evaluateExpression(String str) {
         dataStack =  new GenericStack<Double>();
 		operStack =  new GenericStack<String>();
+		String[] tokens = convertToTokens(str);
 		
-		return ("You need to code this!");
+		if (containsError(tokens) != "") {
+			System.out.println(containsError(tokens));
+			return containsError(tokens);
+		}
+		
+		for (String token : tokens) {
+			if (!token.matches("[\\+\\-\\*\\/\\(\\)]")) {
+				dataStack.push(Double.parseDouble(token));
+			} else {
+				if (!executeOps(token).equals("")) {
+					return DIV0_ERROR;
+				}
+				if (!token.equals(")"))
+					operStack.push(token);
+			}
+		}
+		if (!executeOps("+").equals("")) {
+			return DIV0_ERROR;
+		}
+		return str + " = " + Double.toString(dataStack.pop());
 	}
+	
+	/**
+	 * Executes operations until more operation tokens are needed
+	 *
+	 * @param token
+	 * @return DIV0_ERROR or an empty string
+	 */
+	private String executeOps(String token) {
+		if (operStack.empty())
+			return "";
+		while (isHigherPrecedence(operStack.peek(), token)) {
+			if (operStack.peek().equals("(") && token.equals(")")) {
+				operStack.pop();
+				break;
+			} else {
+				if (!executeTOS().equals("")) {
+					return DIV0_ERROR;
+				}
+			}
+			if (operStack.empty())
+				break;
+		}
+		return "";
+	}
+	
+	/**
+	 * Executes the operation at the top of stack
+	 *
+	 * @param None
+	 * @return DIV0_ERROR or an empty string
+	 */
+	private String executeTOS() {
+		String op = operStack.pop();
+		double a = dataStack.pop();
+		double b = dataStack.pop();
+		if (op.equals("+")) {
+			dataStack.push(b + a);
+		}
+		if (op.equals("-")) {
+			dataStack.push(b - a);
+		}
+		if (op.equals("*")) {
+			dataStack.push(b * a);
+		}
+		if (op.equals("/")) {
+			
+			if (a == 0) {
+				return DIV0_ERROR;
+			}
+			dataStack.push(b / a);
+		}
+		return "";
+	}
+	
 
 }
--- src/GenericStack.java
+++ src/GenericStack.java
@@ -3,25 +3,99 @@ import java.util.EmptyStackException;
 
 // TODO: Auto-generated Javadoc
 /**
- * The Class GenericStack. Implements a generic software stack for any element
- * IMPORTANT: You need to replace with YOUR GenericStack Implementation!!!
- *
+ * The Class GenericStack.
  *
  * @param <E> the element type
  */
-public class GenericStack<E>  {
-	
-	/** The stack.  The stack will be built on a generic ArrayList, but will only
-	 *  expose stack methods push, pop, peek, isEmpty and getSize.
-	 */
+
+/**
+ * @author Your name here
+ *
+ */
+public class GenericStack<E> {
+
+	/** Using an ArrayList as the data structure for the stack */
 	private ArrayList<E> stack;
 	
 	/**
-	 * Instantiates a new generic stack. The stack is empty at the beginning
+	 * Instantiates a new generic stack.
 	 */
 	public GenericStack() {
-		stack = new ArrayList<>();
+		stack = new ArrayList<E>();
 	}
 	
+	/**
+	 * Checks if the stack is empty.
+	 *
+	 * @return true, if the stack is empty
+	 */
+	public boolean empty() {
+		if (stack.size() == 0) {
+			return true;
+		}
+		return false;	
+	}
 
+	/**
+	 * Gets the size of the stack. The Top of Stack is size - 1;
+	 *
+	 * @return the size
+	 */
+	public int size() {
+		//TODO: Update this code
+		return stack.size();
+	}
+
+	/**
+	 * Peek - this is a Java stack function that returns the object at the
+	 * top of the stack without removing it from the stack.
+	 *
+	 * @return the object at the top of the stack
+	 * @throws EmptyStackException if an attempt was made to peek on an empty stack
+	 */
+	public E peek() throws EmptyStackException {
+		if (stack.size() == 0) {
+			throw new EmptyStackException();
+		}
+		return stack.get(stack.size() - 1);
+		//TODO: Update this code
+	}
+
+	/**
+	 * Pops the object off of the top of the stack, and returns it. The ArrayList
+	 * remove method is used to implement this.
+	 *
+	 * @return the object at the top of the stack
+	 * @throws EmptyStackException if an attempt was made to pop on an empty stack
+	 */
+	public E pop() throws EmptyStackException {
+		//TODO: Update this code
+		if (stack.size() == 0) {
+			throw new EmptyStackException();
+		}
+		E pop = this.peek();
+		stack.remove(stack.size() - 1);
+		return pop;
+	}
+	
+	/**
+	 * Pushes an object onto the stack using the ArrayList add method. This also
+	 * adjusts the size of the stack directly...
+	 *
+	 * @param o the object to be added to the stack
+	 */
+	public void push(E o) {
+		//TODO: Update this code
+		stack.add(o);
+	};
+
+	/**
+	 * To string
+	 *
+	 * @return the string
+	 */
+	@Override
+   	public String toString() {
+	   return("stack: "+stack.toString());
+	}
 }
